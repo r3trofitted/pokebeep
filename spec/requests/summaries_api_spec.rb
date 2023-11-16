2927a77b9313f5af524465ea635b42190e900710
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-11-17 00:32:47 +0100

    Removing dead or useless code
    More precisely a useless helper, some useless `let`'s, and useless
    assertion data.

--- spec/requests/summaries_api_spec.rb
+++ spec/requests/summaries_api_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Summaries API' do
   describe 'GET /summaries/{pokemon_id}/{from}/{to}' do
-    let(:first_pokemon) { 1 }
-    let(:second_pokemon) { 2 }
-
     it 'generates summary based on beeps' do
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00', kind: :out)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 16:00', kind: :out)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-02 08:00', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-02 16:00', kind: :out)
   
-      get("/summaries/#{first_pokemon}/2019-05-01/2019-05-30")
+      get("/summaries/1/2019-05-01/2019-05-30")
       summary = JSON.parse(response.body).symbolize_keys
   
       expect(summary).to include({
-                                  pokemon_id: first_pokemon,
+                                  pokemon_id: 1,
                                   from: '2019-05-01', to: '2019-05-30',
                                   presence_hours: 16.0,
                                   problematic_dates: []
@@ -23,44 +20,34 @@ RSpec.describe 'Summaries API' do
     end
   
     it 'rounds presence hours to two decimal places' do
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:10:00:050', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00:01:070', kind: :out)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 08:10:00:050', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 16:00:01:070', kind: :out)
   
-      get("/summaries/#{first_pokemon}/2019-05-01/2019-05-01")
+      get("/summaries/1/2019-05-01/2019-05-01")
       summary = JSON.parse(response.body).symbolize_keys
   
       expect(summary[:presence_hours]).to eq(7.83)
     end
   
     it 'discovers problematic date when there is no leave beep' do
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-02 08:00', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-02 16:00', kind: :out)
       
-      get("/summaries/#{first_pokemon}/2019-05-01/2019-05-30")
+      get("/summaries/1/2019-05-01/2019-05-30")
       summary = JSON.parse(response.body).symbolize_keys
   
-      expect(summary).to include({
-                                  pokemon_id: first_pokemon,
-                                  from: '2019-05-01', to: '2019-05-30',
-                                  presence_hours: 8.0,
-                                  problematic_dates: ['2019-05-01']
-                                })
+      expect(summary).to include(presence_hours: 8.0, problematic_dates: ['2019-05-01'])
     end
   
     it 'discovers problematic date when there is skip day beep' do
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: 1, timestamp: '2019-05-02 16:00', kind: :out)
   
-      get("/summaries/#{first_pokemon}/2019-05-01/2019-05-30")
+      get("/summaries/1/2019-05-01/2019-05-30")
       summary = JSON.parse(response.body).symbolize_keys
   
-      expect(summary).to include({
-                                  pokemon_id: first_pokemon,
-                                  from: '2019-05-01', to: '2019-05-30',
-                                  presence_hours: 0.0,
-                                  problematic_dates: ['2019-05-01', '2019-05-02']
-                                })
+      expect(summary).to include(presence_hours: 0.0, problematic_dates: ['2019-05-01', '2019-05-02'])
     end
     
     describe "validations" do
--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -1,20 +1,5 @@
 module ApiHelpers
   def given_beep(pokemon_id: 1, timestamp: Time.zone.now, kind: :in)
-    Beep.create! pokemon_id:, kind:, timestamp: to_unix(timestamp)
-  end
-
-  private
-
-  def to_unix(timestamp)
-    case timestamp
-    when String
-      Time.parse(timestamp).to_i
-    when Time
-      timestamp.to_i
-    when Number
-      timestamp.to_i
-    else
-      raise ArgumentError, 'Invalid timestamp type. Only String, Time and Number are supported.'
-    end
+    Beep.create! pokemon_id:, kind:, timestamp: Time.parse(timestamp).to_i
   end
 end
