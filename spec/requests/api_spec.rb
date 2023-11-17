cfac2d8400618d767cfb500db24254c3efd3b123
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-11-17 12:57:12 +0100

    (Re)Grouping the examples
    Grouping the examples by the endpoint they cover cost very little
    (especially since the grouping was already done implicitly anyway),
    and help our specs read better, which in turn highlights issues with
    them.

--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -1,60 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe 'public api' do
-  let(:first_pokemon) { 1 }
-  let(:second_pokemon) { 2 }
+  describe 'GET /summaries/{pokemon_id}/{from}/{to}' do
+    let(:first_pokemon) { 1 }
+    let(:second_pokemon) { 2 }
 
-  it 'generates summary based on beeps' do
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00', kind: :out)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
-
-    summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
-
-    expect(summary).to include({
-                                pokemon_id: first_pokemon,
-                                from: '2019-05-01', to: '2019-05-30',
-                                presence_hours: 16.0,
-                                problematic_dates: []
-                              })
-  end
-
-  it 'rounds presence hours to two decimal places' do
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:10:00:050', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00:01:070', kind: :out)
-
-    summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-01')
-
-    expect(summary[:presence_hours]).to eq(7.83)
-  end
-
-  it 'discovers problematic date when there is no leave beep' do
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
-
-    summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
-
-    expect(summary).to include({
-                                pokemon_id: first_pokemon,
-                                from: '2019-05-01', to: '2019-05-30',
-                                presence_hours: 8.0,
-                                problematic_dates: ['2019-05-01']
-                              })
-  end
-
-  it 'discovers problematic date when there is skip day beep' do
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
-    given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
-
-    summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
-
-    expect(summary).to include({
-                                pokemon_id: first_pokemon,
-                                from: '2019-05-01', to: '2019-05-30',
-                                presence_hours: 0.0,
-                                problematic_dates: ['2019-05-01', '2019-05-02']
-                              })
+    it 'generates summary based on beeps' do
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00', kind: :out)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+  
+      summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
+  
+      expect(summary).to include({
+                                  pokemon_id: first_pokemon,
+                                  from: '2019-05-01', to: '2019-05-30',
+                                  presence_hours: 16.0,
+                                  problematic_dates: []
+                                })
+    end
+  
+    it 'rounds presence hours to two decimal places' do
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:10:00:050', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 16:00:01:070', kind: :out)
+  
+      summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-01')
+  
+      expect(summary[:presence_hours]).to eq(7.83)
+    end
+  
+    it 'discovers problematic date when there is no leave beep' do
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 08:00', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+  
+      summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
+  
+      expect(summary).to include({
+                                  pokemon_id: first_pokemon,
+                                  from: '2019-05-01', to: '2019-05-30',
+                                  presence_hours: 8.0,
+                                  problematic_dates: ['2019-05-01']
+                                })
+    end
+  
+    it 'discovers problematic date when there is skip day beep' do
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-01 08:00', kind: :in)
+      given_beep(pokemon_id: first_pokemon, timestamp: '2019-05-02 16:00', kind: :out)
+  
+      summary = summary(pokemon_id: first_pokemon, from: '2019-05-01', to: '2019-05-30')
+  
+      expect(summary).to include({
+                                  pokemon_id: first_pokemon,
+                                  from: '2019-05-01', to: '2019-05-30',
+                                  presence_hours: 0.0,
+                                  problematic_dates: ['2019-05-01', '2019-05-02']
+                                })
+    end
   end
 end
--- spec/requests/api_validation_spec.rb
+++ spec/requests/api_validation_spec.rb
@@ -3,53 +3,57 @@ require 'rails_helper'
 RSpec.describe 'api validation' do
   let(:now) { Time.now.to_i }
 
-  context 'when posting beep with wrong request params' do
-    it 'returns 400 status code' do
-      post_beep({})
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: nil, kind: :in, timestamp: now })
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: 1, kind: nil, timestamp: now })
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: 1, kind: :in, timestamp: nil })
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: 'bad', kind: :in, timestamp: now })
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: 1, kind: 'bad', timestamp: now })
-    end
-
-    it 'returns 400 status code' do
-      post_beep({ pokemon_id: 1, kind: :in, timestamp: 'bad' })
+  describe 'POST /beeps' do
+    context 'when posting beep with wrong request params' do
+      it 'returns 400 status code' do
+        post_beep({})
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: nil, kind: :in, timestamp: now })
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: 1, kind: nil, timestamp: now })
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: 1, kind: :in, timestamp: nil })
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: 'bad', kind: :in, timestamp: now })
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: 1, kind: 'bad', timestamp: now })
+      end
+  
+      it 'returns 400 status code' do
+        post_beep({ pokemon_id: 1, kind: :in, timestamp: 'bad' })
+      end
     end
   end
 
-  context 'when getting summary with wrong request params' do
-    it 'returns 400 status code' do
-      get_summary(1, 'bad', '2019-01-01')
-
-      expect(response.status).to eq(400)
-    end
-
-    it 'returns 400 status code' do
-      get_summary(1, '2019-01-01', 'bad')
-
-      expect(response.status).to eq(400)
-    end
-
-    it 'returns 400 status code' do
-      get_summary('bad', '2019-01-01', '2019-02-02')
-
-      expect(response.status).to eq(400)
+  describe 'GET /summaries/{pokemon_id}/{from}/{to}' do
+    context 'when getting summary with wrong request params' do
+      it 'returns 400 status code' do
+        get_summary(1, 'bad', '2019-01-01')
+  
+        expect(response.status).to eq(400)
+      end
+  
+      it 'returns 400 status code' do
+        get_summary(1, '2019-01-01', 'bad')
+  
+        expect(response.status).to eq(400)
+      end
+  
+      it 'returns 400 status code' do
+        get_summary('bad', '2019-01-01', '2019-02-02')
+  
+        expect(response.status).to eq(400)
+      end
     end
   end
 end
