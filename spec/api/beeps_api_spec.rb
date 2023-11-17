8d1b4f79db61f7b61116ccca45d3329a07400f1f
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-11-17 12:19:40 +0100

    Adding missing specs
    Namely a spec for the happy path of the Beeps API.

--- spec/api/beeps_api_spec.rb
+++ spec/api/beeps_api_spec.rb
@@ -4,39 +4,48 @@ RSpec.describe 'Beeps API' do
   let(:now) { Time.now.to_i }
 
   describe 'POST /beeps' do
-    specify 'posting without any parameter returns a 400 status code' do
-      post('/beeps', params: {})
-      expect(response).to have_http_status(:bad_request)
+    it 'creates a Beep' do
+      expect {
+        post('/beeps', params: { pokemon_id: 1, kind: :in, timestamp: now })
+      }.to change { Beep.count }.by(1)
+      expect(response).to have_http_status(:ok)
     end
-
-    specify 'posting without a :pokemon_id parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: nil, kind: :in, timestamp: now })
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    specify 'posting without a :kind parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: 1, kind: nil, timestamp: now })
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    specify 'posting without a :timestamp parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: 1, kind: :in, timestamp: nil })
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    specify 'posting with an invalid :pokemon_id parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: 'bad', kind: :in, timestamp: now })
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    specify 'posting with an invalid :kind parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: 1, kind: 'bad', timestamp: now })
-      expect(response).to have_http_status(:bad_request)
-    end
-
-    specify 'posting with an invalid :timestamp parameter returns a 400 status code' do
-      post('/beeps', params: { pokemon_id: 1, kind: :in, timestamp: 'bad' })
-      expect(response).to have_http_status(:bad_request)
+    
+    describe 'validations' do
+      specify 'posting without any parameter returns a 400 status code' do
+        post('/beeps', params: {})
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting without a :pokemon_id parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: nil, kind: :in, timestamp: now })
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting without a :kind parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: 1, kind: nil, timestamp: now })
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting without a :timestamp parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: 1, kind: :in, timestamp: nil })
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting with an invalid :pokemon_id parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: 'bad', kind: :in, timestamp: now })
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting with an invalid :kind parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: 1, kind: 'bad', timestamp: now })
+        expect(response).to have_http_status(:bad_request)
+      end
+  
+      specify 'posting with an invalid :timestamp parameter returns a 400 status code' do
+        post('/beeps', params: { pokemon_id: 1, kind: :in, timestamp: 'bad' })
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 end
